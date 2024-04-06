05a3047a5e01e245ab903efae788c3ae8f74e06d
Naveenvk29 <naveenvinodkumar29@gmail.com>
2024-04-07 00:04:15 +0530

    Added user-controllers

--- server/Controllers/user.controllers.js
+++ server/Controllers/user.controllers.js
@@ -37,4 +37,71 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email: email });
+
+  if (user && user.isPasswordCorrect(password)) {
+    generateToken(res, user._id);
+
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      message: "User Logged In Successfully",
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Credentials");
+  }
+});
+
+const logout = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({
+    message: "User Logged Out Successfully",
+  });
+});
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+
+  if (user) {
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      username: updatedUser.username,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
+
+export { registerUser, loginUser, logout, getUserProfile, updateUserProfile };
--- server/Routes/user.routes.js
+++ server/Routes/user.routes.js
@@ -1,8 +1,21 @@
 import express from "express";
-import { registerUser } from "../Controllers/user.controllers.js";
+import {
+  registerUser,
+  loginUser,
+  logout,
+  getUserProfile,
+  updateUserProfile,
+} from "../Controllers/user.controllers.js";
+import protect from "../Middleware/auth.Middleware.js";
 
 const router = express.Router();
 
 router.post("/", registerUser);
+router.post("/login", loginUser);
+router.post("/logout", logout);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
