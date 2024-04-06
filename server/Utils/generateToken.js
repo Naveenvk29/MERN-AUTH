b060a041fcd68ef6289abd6ee5711c5536b2963e
Naveenvk29 <naveenvinodkumar29@gmail.com>
2024-04-06 23:14:50 +0530

    model & generate token add

---
+++ server/Utils/generateToken.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "3d",
+  });
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    sameSite: true,
+    secure: process.env.NODE_ENV !== "development",
+    maxAge: 3 * 24 * 60 * 60 * 1000,
+  });
+};
+
+export default generateToken;
